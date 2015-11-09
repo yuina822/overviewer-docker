29e2c911da1abc29f77986f4054fc877b18bd40e
Yuichi NAGAYAMA <ayuichi@club.kyutech.ac.jp>
2015-11-10 01:27:30 +0900

    Replace watch command with infinite loop by "while true" and add some environment variables which you can set your texturepack, map path, minecraft world path and your world name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+
 if [ -z $OVERVIEWER_CYCLE ]; then
-		OVERVIEWER_CYCLE=86400
+	export OVERVIEWER_CYCLE=3600
 fi
 
-watch -n $OVERVIEWER_CYCLE /usr/src/app/overviewer/overviewer.py --config="/config.py"
+if [ -z $OVERVIEWER_WORLD_PATH ]; then
+	export OVERVIEWER_WORLD_PATH="/data/world"
+fi
+
+if [ -z $OVERVIEWER_MAP_PATH ]; then
+	export OVERVIEWER_MAP_PATH="/usr/share/nginx/html"
+fi
+
+if [ -z $OVERVIEWER_TEXTURE_URL ]; then
+	export OVERVIEWER_TEXTURE_URL="https://s3.amazonaws.com/Minecraft.Download/versions/1.8.8/1.8.8.jar"
+fi
+
+if [ -z $OVERVIEWER_WORLD_NAME ]; then
+	export OVERVIEWER_WORLD_NAME="myworld"
+fi
+
+wget $OVERVIEWER_TEXTURE_URL -O /texture
+
+while true; do
+	/usr/src/app/overviewer/overviewer.py --config="/config.py" --genpoi
+	/usr/src/app/overviewer/overviewer.py --config="/config.py"
+	sleep $OVERVIEWER_CYCLE
+done
