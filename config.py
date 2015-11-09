6e935ca491330d8e5d3879565ab720f3364ef7fb
Yuichi NAGAYAMA <ayuichi@club.kyutech.ac.jp>
2015-11-10 01:24:34 +0900

    Add all views from left or right, and day or night and popup where is players at last.

--- config.py
+++ config.py
@@ -1,31 +1,86 @@
 #!/usr/bin/env python
 import os
 
+def playerIcons(poi):
+    if poi['id'] == 'Player':
+        poi['icon'] = "https://minotar.net/helm/%s/32" % poi['EntityId']
+        return "Last known location for %s" % poi['EntityId']
+
 world_path = os.environ.get('OVERVIEWER_WORLD_PATH')
-if not world_path :
-    world_path = "/data/world"
+texturepath = '/texture'
 
-outputdir = os.environ.get('OVERVIEWER_MAP_PATH')
-if not outputdir :
-    outputdir = "/mcmap"
+world_name = os.environ.get('OVERVIEWER_WORLD_NAME')
 
-renders["day"] = {
-    "world": "myworld",
-    "title": "Daytime",
-    "rendermode": smooth_lighting,
-    "dimension": "overworld",
+worlds[world_name] = world_path
+
+renders['day-left'] = {
+    'world': world_name,
+    'title': 'Day(left)',
+    'rendermode': smooth_lighting,
+    'dimension': 'overworld',
+    'northdirection': 'upper-left',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
 }
 
-renders["night"] = {
-    "world": "myworld",
-    "title": "Nighttime",
-    "rendermode": smooth_night,
-    "dimension": "overworld",
+renders['day-right'] = {
+    'world': world_name,
+    'title': 'Day(right)',
+    'rendermode': smooth_lighting,
+    'dimension': 'overworld',
+    'northdirection': 'upper-right',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
 }
 
-renders["nether"] = {
-    "world": "myworld",
-    "title": "Nether",
-    "rendermode": nether_smooth_lighting,
-    "dimension": "nether",
+renders['night-left'] = {
+    'world': world_name,
+    'title': 'Night(left)',
+    'rendermode': smooth_night,
+    'dimension': 'overworld',
+    'northdirection': 'upper-left',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
+}
+
+renders['night-right'] = {
+    'world': world_name,
+    'title': 'Night(right)',
+    'rendermode': smooth_night,
+    'dimension': 'overworld',
+    'northdirection': 'upper-right',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
+}
+
+renders['nether-left'] = {
+    'world': world_name,
+    'title': 'Nether(left)',
+    'rendermode': nether_smooth_lighting,
+    'dimension': 'nether',
+    'northdirection': 'upper-left',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
+}
+
+renders['nether-right'] = {
+    'world': world_name,
+    'title': 'Nether(right)',
+    'rendermode': nether_smooth_lighting,
+    'dimension': 'nether',
+    'northdirection': 'upper-right',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
+}
+
+renders['end-left'] = {
+    'world': world_name,
+    'title': 'The End(left)',
+    'rendermode': smooth_lighting,
+    'dimension': 'end',
+    'northdirection': 'upper-left',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
+}
+
+renders['end-right'] = {
+    'world': world_name,
+    'title': 'The End(right)',
+    'rendermode': smooth_lighting,
+    'dimension': 'end',
+    'northdirection': 'upper-right',
+    'markers': [dict(name='Players', filterFunction=playerIcons, checked=True)],
 }
